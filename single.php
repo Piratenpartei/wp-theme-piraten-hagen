a128c92f7ef0820157eb2104c836df0c1cb34d67
Chris Jung <campino2k@gmail.com>
2011-01-12 16:19:37 +0100

    Remove PDF-Verseion Link

--- single.php
+++ single.php
@@ -4,7 +4,6 @@
 			<div class="post">
 				<div style="text-align:right">
 				<?php if( $post -> ping_status == 'open' ) { ?><a href="<?php trackback_url(display); ?> ">Trackback</a><?php } ?><?php if( $post -> comment_status == 'open' ) { ?> <a href="#comments">Kommentare</a><?php } ?>
-				<a href="?article2pdf=1">PDF Version</a>
 				</div>
 					<?php require('post.php'); ?>
 					<?php comments_template(); ?>
