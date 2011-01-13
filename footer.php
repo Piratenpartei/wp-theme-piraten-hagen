e7ec839c800b1eb55b1e092c4287aa998c4f9b67
Chris Jung <campino2k@gmail.com>
2011-01-13 21:27:31 +0100

    Put widgets in Footer
    Replace the hard coded widget columns by registered sidebars

--- footer.php
+++ footer.php
@@ -1,3 +1,40 @@
-<?php
-	require( '3_footer.php' );
-?>
+		<div id="rechts">
+			<div id="sidebar_right" class="sidebar">
+			<?php require( 'sidebar_right.php' ); ?>
+			</div>
+		</div>
+	</div>
+	<div id="unten">
+
+	</div>
+	<div id="fussoben"></div>
+	<div id="fussmitte">
+		<div id="fussl"><!-- links aussen -->
+			<!-- Widget 1 -->
+			<?php dynamic_sidebar( 'footer-widget-area-01' ); ?>
+		</div>
+		<div id="fusslm"><!-- links mittig -->
+			<!-- Widget 2 -->
+			<?php dynamic_sidebar( 'footer-widget-area-02' ); ?>
+		</div>
+		<div id="fussrm"><!-- rechts aussen -->
+			<!-- Widget 4 -->
+			<?php dynamic_sidebar( 'footer-widget-area-04' ); ?>
+		</div>
+		<div id="fussr"><!-- rechts mittig -->
+			<!-- Widget 3 -->
+			<?php dynamic_sidebar( 'footer-widget-area-03' ); ?>
+		</div>
+	</div>
+	<div id="fussunten"></div>
+	<div id="fusslinie">
+		<a href="/kontakt">Kontakt</a>&nbsp;&#x25cf; <a href="/impressum">Impressum</a>&nbsp;&#x25cf; <a href="http://www.piratenpartei.de/">Piratenpartei Deutschland</a>&nbsp;&#x25cf; <a href="http://piratenpartei-hessen.de/">Piratenpartei Landesverband Hessen</a>&nbsp;&#x25cf; <a href="http://www.junge-piraten.de/">Junge Piraten</a>
+		<div id="serverinfo">
+			WordPress mit <em>piratenhagen</em> bzw. <em>piratenmk</em> Theme<br/>
+			<a href="http://wiki.piratenpartei.de/images/6/62/We_.jpg">Design</a> von <a href="http://wiki.piratenpartei.de/Benutzer:Seeki">Seeki</a>, umgesetzt von <a href="http://wiki.piratenpartei.de/Benutzer:hatch">hatch</a> und <a href="">Tilli</a> und den <a href="http://piraten-hagen.de/">Piraten Hagen</a>, WordPress Theme von <a href="http://wiki.piratenpartei.de/Benutzer:motorradblogger">motorradblogger</a>, <a href="http://piratenpartei-iserlohn.de">Lenne Piraten</a>, <a href="http://campino2k.de">campino2k</a>
+		</div>
+		<?php wp_footer(); ?>
+	</div>
+</div>
+</body>
+</html>
--- functions.php
+++ functions.php
@@ -11,10 +11,56 @@ function theme_setup() {
 	if ( ! isset( $content_width ) )
 		$content_width = 500;
 
-	if( function_exists('register_sidebar') )
-		register_sidebar( array( 'name' => 'sidebar_full_left' ) );
-	if( function_exists('register_sidebar') )
-		register_sidebar( array( 'name' => 'sidebar_full_right' ) );
 
 }
+
+add_action( 'widgets_init', 'theme_widgets_setup' );
+function theme_widgets_setup() {
+
+	register_sidebar( array(
+		'name' => 'Linke Sidebar',
+		'id' => 'sidebar_full_left' 
+	) );
+	register_sidebar( array(
+		'name' => 'Rechte Sidebar',
+		'id' => 'sidebar_full_right' 
+	) );
+
+	register_sidebar( array(
+		'name' => 'Footer Widget Area 01',
+		'id' => 'footer-widget-area-01',
+		'description' => 'Erste von 4 Footer-Spalten',
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<strong>',
+		'after_title' => '</strong>',
+	) );
+	register_sidebar( array(
+		'name' => 'Footer Widget Area 02',
+		'id' => 'footer-widget-area-02',
+		'description' => 'Zweite von 4 Footer-Spalten',
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<strong>',
+		'after_title' => '</strong>',
+	) );
+	register_sidebar( array(
+		'name' => 'Footer Widget Area 03',
+		'id' => 'footer-widget-area-03',
+		'description' => 'Dritte von 4 Footer-Spalten',
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<strong>',
+		'after_title' => '</strong>',
+	) );
+	register_sidebar( array(
+		'name' => 'Footer Widget Area 04',
+		'id' => 'footer-widget-area-04',
+		'description' => 'Vierte von 4 Footer-Spalten',
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<strong>',
+		'after_title' => '</strong>',
+	) );
+};
 ?>
