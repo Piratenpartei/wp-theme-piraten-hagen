726271d38c03a2d64848165471d0258583869c1a
Thomas Ulrich <pirat.wm@googlemail.com>
2012-03-17 13:59:05 +0100

    Removed unnecessary files 	deleted:    1_heimathafen.php 	deleted:    2_menu.php 	deleted:    3_footer.php

--- 1_heimathafen.php
+++
@@ -1,6 +0,0 @@
-
-<ul class="menu">
-	<li class="leaf"><a href="/wp-admin/" title="Nur für Mitglieder: Am WordPress anmelden">Admin</a></li>
-	<li class="leaf"><a href="http://wiki.piratenpartei.de/" title="Zum Wiki der Piratenpartei wechseln.">Wiki</a></li>
-	<li class="leaf"><a href="http://forum.piratenpartei.de/" title="Zum Forum der Piratenpartei wechseln.">Forum</a></li>
-</ul>
